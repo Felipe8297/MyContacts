8d8b1418886c5419e5013f7a4a0fbe4b620c7e8d
Felipe Soares <felipe8297@gmail.com>
2025-05-31 09:48:36 -0300

    refactor: enhance findAll method to include category names and improve query structure

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -5,7 +5,11 @@ class ContactsRepository {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
 
     const rows = await db.query(
-      `SELECT * FROM contacts ORDER BY name ${direction}`,
+      `SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      ORDER BY contacts.name ${direction}
+      `,
     );
 
     return rows;
